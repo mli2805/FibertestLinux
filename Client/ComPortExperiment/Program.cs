0039c4462861ee0b4172fddcf8b55ea2b5924614
mli2805 <mli2805@gmail.com>
2022-11-11 12:48:40 +0300

    + TcpExp

--- Client/ComPortExperiment/Program.cs
+++ Client/ComPortExperiment/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO.Ports;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ComPortExperiment
 {
@@ -8,6 +10,30 @@ namespace ComPortExperiment
         {
             Console.WriteLine("Hello, World!");
 
+            //SerialPort();
+            TcpExp();
+
+            Console.ReadKey();
+        }
+
+        private static void TcpExp()
+        {
+            var cmd = "get_rtu_number\r\n";
+            var client = new TcpClient();
+            client.Connect("192.168.88.101", 23);
+            var nwStream = client.GetStream();
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(cmd);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+            Thread.Sleep(TimeSpan.FromMilliseconds(200));
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            var answer = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            client.Close();
+            Console.WriteLine(answer);
+        }
+
+        private static void SerialPort()
+        {
             var serialPort = new SerialPort("COM2", 115200);
             try
             {
