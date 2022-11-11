dc3cfe39437f89fa748a490e15a7b42f97555b5d
mli2805 <mli2805@gmail.com>
2022-11-11 09:49:35 +0300

    + ComPortExperiment

---
+++ Client/ComPortExperiment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Ports;
+
+namespace ComPortExperiment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+
+            var serialPort = new SerialPort("COM2", 115200);
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            Console.WriteLine($"Now RTS is {serialPort.RtsEnable}");
+            serialPort.RtsEnable = !serialPort.RtsEnable;
+            Thread.Sleep(10);
+            Console.WriteLine($"Now RTS is {serialPort.RtsEnable}");
+            serialPort.RtsEnable = !serialPort.RtsEnable;
+            Thread.Sleep(10);
+            Console.WriteLine($"Now RTS is {serialPort.RtsEnable}");
+
+            serialPort.Close();
+        }
+    }
+}
