0f4fcad3f32e2bb183b4d4cfeb0f1665d275d9c4
root <mli2805@gmail.com>
2022-11-30 10:15:15 +0300

    scripts improved

--- copyRtu56.sh
+++ copyRtu56.sh
@@ -3,4 +3,4 @@
 echo запускаем из каталога солюшена
 
 pscp ./Deploy/rtu.tar.gz leanid@192.168.96.56:/var/tmp
-pscp install.sh leanid@192.168.96.56:/var/tmp
+#pscp install.sh leanid@192.168.96.56:/var/tmp
